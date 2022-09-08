6bcd64c540c28171f7226439b7596c325eb5437d
satyazmx <thakursatya6@gmail.com>
2022-09-08 17:57:58 +0530

    add project directory

---
+++ forest_cover/component/data_ingestion.py
@@ -0,0 +1,108 @@
+from forest_cover.entity.config_entity import DataIngestionConfig
+import sys,os
+from forest_cover.exception import ForestException
+from forest_cover.logger import logging
+from forest_cover.entity.artifact_entity import DataIngestionArtifact
+import tarfile
+import numpy as np
+from six.moves import urllib
+import zipfile
+import pandas as pd
+import shutil
+from sklearn.model_selection import StratifiedShuffleSplit
+
+class DataIngestion:
+
+    def __init__(self,data_ingestion_config:DataIngestionConfig ):
+        try:
+            logging.info(f"{'>>'*20}Data Ingestion log started.{'<<'*20} ")
+            self.data_ingestion_config = data_ingestion_config
+
+        except Exception as e:
+            raise ForestException(e,sys)
+    
+
+    def extract_tgz_file(self,tgz_file_path:str):
+        try:
+            
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+
+            if os.path.exists(raw_data_dir):
+                os.remove(raw_data_dir)
+
+            os.makedirs(raw_data_dir,exist_ok=True)
+
+            logging.info(f"Extracting tgz file: [{tgz_file_path}] into dir: [{raw_data_dir}]")
+
+            with zipfile.ZipFile(tgz_file_path, 'r') as zip_ref:
+                zip_ref.extractall(raw_data_dir)
+            logging.info(f"Extracting zip file: {tgz_file_path} into dir: {raw_data_dir}")
+
+        except Exception as e:
+            raise ForestException(e,sys) from e
+    
+    def split_data_as_train_test(self) -> DataIngestionArtifact:
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+
+            file_name = os.listdir(raw_data_dir)[0]
+
+            housing_file_path = os.path.join(raw_data_dir,file_name)
+
+            logging.info(f"Reading csv file: [{housing_file_path}]")
+            housing_data_frame = pd.read_csv(housing_file_path)
+
+            housing_data_frame["income_cat"] = pd.cut(
+                housing_data_frame["Cover_Type"],
+                bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+                labels=[1,2,3,4,5]
+            )
+            
+            logging.info(f"Splitting data into train and test")
+            strat_train_set = None
+            strat_test_set = None
+
+            split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+            for train_index,test_index in split.split(housing_data_frame, housing_data_frame["income_cat"]):
+                strat_train_set = housing_data_frame.loc[train_index].drop(["income_cat"],axis=1)
+                strat_test_set = housing_data_frame.loc[test_index].drop(["income_cat"],axis=1)
+
+            train_file_path = os.path.join(self.data_ingestion_config.ingested_train_dir,
+                                            file_name)
+
+            test_file_path = os.path.join(self.data_ingestion_config.ingested_test_dir,
+                                        file_name)
+            
+            if strat_train_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_train_dir,exist_ok=True)
+                logging.info(f"Exporting training datset to file: [{train_file_path}]")
+                strat_train_set.to_csv(train_file_path,index=False)
+
+            if strat_test_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_test_dir, exist_ok= True)
+                logging.info(f"Exporting test dataset to file: [{test_file_path}]")
+                strat_test_set.to_csv(test_file_path,index=False)
+            
+
+            data_ingestion_artifact = DataIngestionArtifact(train_file_path=train_file_path,
+                                test_file_path=test_file_path,
+                                is_ingested=True,
+                                message=f"Data ingestion completed successfully."
+                                )
+            logging.info(f"Data Ingestion artifact:[{data_ingestion_artifact}]")
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise ForestException(e,sys) from e
+
+    def initiate_data_ingestion(self)-> DataIngestionArtifact:
+        try:
+            tgz_file_path = self.data_ingestion_config.zip_download_dir
+            self.extract_tgz_file(tgz_file_path=tgz_file_path)
+            return self.split_data_as_train_test()
+        except Exception as e:
+            raise ForestException(e,sys) from e
+
+    def __del__(self):
+        logging.info(f"{'>>'*20}Data Ingestion log completed.{'<<'*20} \n\n")
---
+++ forest_cover/component/data_transformation.py
@@ -0,0 +1,105 @@
+from sklearn import preprocessing
+from forest_cover.exception import ForestException
+from forest_cover.logger import logging
+from forest_cover.entity.config_entity import DataTransformationConfig 
+from forest_cover.entity.artifact_entity import DataIngestionArtifact,\
+DataValidationArtifact,DataTransformationArtifact
+import sys,os
+import numpy as np
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+import pandas as pd
+from forest_cover.constant import *
+from forest_cover.util.util import read_json_file, read_yaml_file,save_object,save_numpy_array_data,load_data
+import pickle
+
+class DataTransformation:
+
+    def __init__(self, data_transformation_config: DataTransformationConfig,
+                 data_ingestion_artifact: DataIngestionArtifact,
+                 data_validation_artifact: DataValidationArtifact
+                 ):
+        try:
+            logging.info(f"{'>>' * 30}Data Transformation log started.{'<<' * 30} ")
+            self.data_transformation_config= data_transformation_config
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_artifact = data_validation_artifact
+
+        except Exception as e:
+            raise ForestException(e,sys) from e
+
+    
+    def initiate_data_transformation(self)->DataTransformationArtifact:
+        try:
+        
+            logging.info(f"Obtaining training and test file path.")
+            train_file_path = self.data_ingestion_artifact.train_file_path
+            test_file_path = self.data_ingestion_artifact.test_file_path
+            
+
+            schema_file_path = self.data_validation_artifact.schema_file_path
+            
+            logging.info(f"Loading training and test data as pandas dataframe.")
+            train_df = load_data(file_path=train_file_path, schema_file_path=schema_file_path)
+            
+            test_df = load_data(file_path=test_file_path, schema_file_path=schema_file_path)
+
+            schema = read_yaml_file(file_path=schema_file_path)
+
+            target_column_name = schema[TARGET_COLUMN_KEY]
+
+
+            logging.info(f"Splitting input and target feature from training and testing dataframe.")
+            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            # Testing created Pipe
+            pickle_model = pickle.load(open('pr.pkl','rb'))
+
+            input_feature_train_arr=pickle_model.fit_transform(input_feature_train_df)
+            input_feature_test_arr = pickle_model.transform(input_feature_test_df)
+
+            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe")
+        
+            train_arr = np.c_[ input_feature_train_arr, np.array(target_feature_train_df)]
+
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            
+            transformed_train_dir = self.data_transformation_config.transformed_train_dir
+            transformed_test_dir = self.data_transformation_config.transformed_test_dir
+
+            train_file_name = os.path.basename(train_file_path).replace(".csv",".npz")
+            test_file_name = os.path.basename(test_file_path).replace(".csv",".npz")
+
+            transformed_train_file_path = os.path.join(transformed_train_dir, train_file_name)
+            transformed_test_file_path = os.path.join(transformed_test_dir, test_file_name)
+
+            logging.info(f"Saving transformed training and testing array.")
+            
+            save_numpy_array_data(file_path=transformed_train_file_path,array=train_arr)
+            save_numpy_array_data(file_path=transformed_test_file_path,array=test_arr)
+
+            preprocessing_obj_file_path = self.data_transformation_config.preprocessed_object_file_path
+
+            logging.info(f"Saving preprocessing object.")
+            #save_object(file_path=preprocessing_obj_file_path,obj=preprocessing_obj)
+
+            data_transformation_artifact = DataTransformationArtifact(is_transformed=True,
+            message="Data transformation successfull.",
+            transformed_train_file_path=transformed_train_file_path,
+            transformed_test_file_path=transformed_test_file_path,
+            preprocessed_object_file_path=preprocessing_obj_file_path
+
+            )
+            logging.info(f"Data transformationa artifact: {data_transformation_artifact}")
+            return data_transformation_artifact
+        except Exception as e:
+            raise ForestException(e,sys) from e
+
+    def __del__(self):
+        logging.info(f"{'>>'*30}Data Transformation log completed.{'<<'*30} \n\n")
